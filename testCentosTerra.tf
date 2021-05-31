c0a47734be69bbfdde2ac8bbc0eac00e212f16b2
tdufau000 <85114144+tdufau000@users.noreply.github.com>
2021-05-31 16:15:37 +0200

    Update testCentosTerra.tf

--- testCentosTerra.tf
+++ testCentosTerra.tf
@@ -6,7 +6,6 @@ provider "aws" {
 resource "aws_instance" "server1" {
    ami           = "ami-045fa58af83eb0ff4"
    instance_type = "t2.micro"
-   key_name      = "key-pair-morphAWS"
 }
 
 resource "aws_default_vpc" "default" {
