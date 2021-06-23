5c3706562d35e86b652d52ebe127799d2d113e4e
tdufau000 <85114144+tdufau000@users.noreply.github.com>
2021-06-23 17:14:40 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -11,4 +11,9 @@ provider "aws" {
   region     = "eu-west-1"
   access_key = var.access_key
   secret_key = var.secret_key
+  
+  assume_role {
+    # The role ARN within Account B to AssumeRole into.
+    role_arn = "arn:aws:iam::161331958293:role/OrganizationAccountAccessRole"
+  }
 }
